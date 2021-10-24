a00b4d772ee75b1ce574b3868ac9b827c0e20637
RodrigoFernandes79 <rodrigohf79@hotmail.com>
2021-10-24 15:26:40 -0300

    Testando a primeira classe de domínio - Categoria

---
+++ src/main/java/com/rodrigofernandes/projetospringboot/domain/Categoria.java
@@ -0,0 +1,49 @@
+package com.rodrigofernandes.projetospringboot.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+	
+
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String nome;
+	
+	
+	public Categoria() {
+		
+	}
+
+
+	public Categoria(Long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+	
+
+}
--- src/main/java/com/rodrigofernandes/projetospringboot/resource/CategoriaResource.java
+++ src/main/java/com/rodrigofernandes/projetospringboot/resource/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.rodrigofernandes.projetospringboot.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigofernandes.projetospringboot.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listAll() {
-		return "REST API está funcionando!";
+	public List<Categoria>  listAll(){
+		Categoria c1 = new Categoria(1L, "Informática");
+		Categoria c2 = new Categoria(2L, "Escritório");
+		List<Categoria> lista  =new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		return  lista;
+		
+		
+		
 	}
 
 }
